2256c81da6be5c5d0f34416d9fd0e0d77699eaff
Chris Lowe <clowe@luc.edu>
2021-01-29 11:54:18 -0700

    Sentry integration

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ import reportWebVitals from "./reportWebVitals";
 import store from "./store/index";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://53762b96badf4af5a85b494332e73aa3@o141824.ingest.sentry.io/5614947",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
