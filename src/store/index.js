c9e9de015517b0ca42126a7267821316ab375975
Chris Lowe <clowe@luc.edu>
2023-08-07 13:30:00 -0600

    fix type-o

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,18 @@
 import { createStore, combineReducers } from "redux";
 import workoutListReducer from "./reducers/workoutListReducer";
 import activityReducer from "./reducers/activityReducer";
-// import workouts from "../api/demoWorkouts.json";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient("https://iekzfniuwsvsrfewgohg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlla3pmbml1d3N2c3JmZXdnb2hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE0MjQzMTIsImV4cCI6MjAwNzAwMDMxMn0.0izcEnH86wcnLme6jjF_PMNFqcBm0jc9xgPer0zqRPE");
+const supabaseRestEndpoint = process.env.REACT_APP_SUPABASE_REST;
+const supabaseApiKey = process.env.REACT_APP_SUPABASE_API_KEY;
+const supabase = createClient(supabaseRestEndpoint, supabaseApiKey);
 
 //Combine our reducers and change property names
 const allReducers = combineReducers({
   workouts: workoutListReducer,
   activities: activityReducer,
 });
-
-// //Create initial State.
-// const preloadedState = {
-//   workouts: workouts,
-//   activities: getActivities(),
-// };
-
 
 async function fetchActivities() {
   const { data, error } = await supabase.from("activity").select();
