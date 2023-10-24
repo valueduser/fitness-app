05d1e837eaeaf7eae63affcd9a9a9e43ce7633e0
valueduser <valueduser@users.noreply.github.com>
2023-10-24 11:13:06 -0600

    Update supabaseClient to prevent empty strings

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseRestEndpoint: string = process.env.REACT_APP_SUPABASE_REST || "";
-const supabaseApiKey: string = process.env.REACT_APP_SUPABASE_API_KEY || "";
+const supabaseRestEndpoint: string = process.env.REACT_APP_SUPABASE_REST;
+const supabaseApiKey: string = process.env.REACT_APP_SUPABASE_API_KEY;
 const supabaseClient = createClient(supabaseRestEndpoint, supabaseApiKey);
 
 export default supabaseClient;
